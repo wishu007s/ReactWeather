0ac6aa50258f0a6df19573a1ca4bc61c68a8ab7b
viswanath007 <bloggerfree4u@gmail.com>
2017-05-02 19:02:02 +0000

    Update copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,7 @@ var React       = require("react");
 
 var Examples    = (props) => {
         return (
-            <h2>Examples Component</h2>
+            <h2>Examples Component!</h2>
             );
 };
 
