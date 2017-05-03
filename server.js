b86072b798b30e30a4455d57c3157583ced01161
viswanath007 <bloggerfree4u@gmail.com>
2017-05-03 09:28:21 +0000

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 
 var app     = express();
+const PORT    = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Express is running up on localhost");
+app.listen(PORT, process.env.IP, function(){
+   console.log("Express server is up on port " + PORT);
 });
